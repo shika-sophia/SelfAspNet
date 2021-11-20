f8521d9fad4156aa65df9c5af51efa62c1b70163
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-11-21 05:12:59 +0900

    2021-11-21-0500

---
+++ Models/DocTemplate.cs
@@ -0,0 +1,22 @@
+﻿/**
+ *@title SelfAspNet / SampleAsp /
+ *@target
+ *@reference 山田祥寛『独習 ASP.NET 第６版』翔泳社, 2020
+ *@content
+ *@subject
+ *
+ *@author shika
+ *@date
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SelfAspNet.Models
+{
+    public class DocTemplate
+    {
+        
+    }//class
+}
