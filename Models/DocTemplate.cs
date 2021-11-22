30dbaca948b6b75bd5078620b9e0e9321aa8e778
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-11-22 15:40:42 +0900

    2021-11-22-1530

--- Models/DocTemplate.cs
+++ Models/DocTemplate.cs
@@ -17,6 +17,17 @@ namespace SelfAspNet.Models
 {
     public class DocTemplate
     {
-        
+        //static void Main(string[] args)
+        public void Main(string[] args)
+        {
+            //new CsharpBegin.Utility.FileDocumentDiv.FileDocExecute().ReadWriteExe();
+        }//Main()
     }//class
 }
+
+/*
+【考察】SelfAspNet -> CsharpBegin参照
+クラスそのものは参照可だが、static Main()で実行しても、
+CsharpBegin内には static Main()がないため、ビルドエラーとなる。
+
+ */
