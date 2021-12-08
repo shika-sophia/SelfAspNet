02bb0d9504e7c128c6c3ca6de174bbd250962b58
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-12-08 12:01:20 +0900

    2021-12-08-1200

---
+++ SampleAsp/NT05_DataSourceControl/NestedGrid.aspx.cs
@@ -0,0 +1,48 @@
+﻿/** <!--
+ *@title SelfAspNet / SampleAsp / NT05_DataSourceControl / NeatedGrid.aspx.cs
+ *@target NeatedGrid.aspx
+ *@source SelfAspDB_NeatedGrid         / Album / SELECT category
+ *@source SelfAspDB_SqlPlaceHolderGrid / Album / SELECT * WHERE category = @category
+ *@reference 山田祥寛『独習 ASP.NET 第６版』翔泳社, 2020
+ *@content 5.1.3 NestedGrid 階層的なグリッド表 / p214
+ *@subject ----- outer GridView ----
+ *         category, <TemplateField> inner GridView
+ *         
+ *@subject ----- inner GridView ----
+ *         内側のGridは、WHERE句の @categoryに
+ *         HiddenFieldの categoryの値を代入することで絞り込み。
+ *         
+ *         <ItemField>
+ *           <HiddenField> ID="hdnCaterory" Value="<%# Eval("category') %>
+ *           <GridView> 5.1.1 SqlPlaceHolderと同様(コピー)
+ *           <DataSourceControl>
+ *             SELECT * FROM Album WHERE (category=@category);
+ *           <SelectParameters>
+ *             <asp:ControlParameter
+ *                  ControlID="hdnCaterory" 
+ *                  Name="category"
+ *                  PropertyName="Value"
+ *                  Type="String" />
+ *                  
+ *@see ResultFile / NestedGrid.jpg
+ *@author shika
+ *@date 2021-12-08
+ * -->
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SelfAspNet.SampleAsp.NT05_DataSourceControl
+{
+    public partial class NestedGrid : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }//class
+}
---
+++ SampleAsp/NT05_DataSourceControl/NestedGrid.aspx.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <自動生成>
+//     このコードはツールによって生成されました。
+//
+//     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+//     コードが再生成されるときに損失したりします。 
+// </自動生成>
+//------------------------------------------------------------------------------
+
+namespace SelfAspNet.SampleAsp.NT05_DataSourceControl
+{
+
+
+    public partial class NestedGrid
+    {
+
+        /// <summary>
+        /// formNestedGrid コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm formNestedGrid;
+
+        /// <summary>
+        /// outerGrid コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView outerGrid;
+
+        /// <summary>
+        /// SelfAspDB_NestedGrid コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SelfAspDB_NestedGrid;
+    }
+}
