36bc007f6352d7ef94a9710dad4f40d49b251921
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-12-27 13:44:54 +0900

    2021-12-27-1330

--- SampleAsp/NT05_DataSourceControl/TypedDataSet/AlbumTableAdapter.cs
+++ SampleAsp/NT05_DataSourceControl/TypedDataSet/AlbumTableAdapter.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/**
+ *@title SelfAspNet / SampleAsp /
+ *@target
+ *@source
+ *@reference 山田祥寛『独習 ASP.NET 第６版』翔泳社, 2020
+ *@content
+ *@subject
+ *
+ *@author shika
+ *@date
+ */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
