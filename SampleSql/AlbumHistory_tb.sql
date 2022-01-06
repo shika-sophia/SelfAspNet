2cb193f66e4a694cd5b6fc61ffeb2260531283b3
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-01-06 16:40:07 +0900

    2022-01-06-1630

---
+++ SampleSql/AlbumHistory_tb.sql
@@ -0,0 +1,17 @@
+﻿/*
+ *@title SelfAspNet / SampleSql / AlbumHistory_tb.sql
+ */
+/*
+-- Create Table 
+CREATE TABLE [dbo].[AlbumHistory]
+(
+	[history] INT NOT NULL PRIMARY KEY IDENTITY,
+	[id] CHAR(5) NOT NULL, 
+    [comment] NVARCHAR(100) NOT NULL, 
+    [lastMod] DATETIME NOT NULL DEFAULT GETDATE(),
+)
+
+-- AlbumDataSet.xsd / AlbumTableAdapter
+INSERT INTO [AlbumHistory] ([Id], [comment], [lastMod])
+  VALUES (@Id, @comment, GETDATE))
+*/
