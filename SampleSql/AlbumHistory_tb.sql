eaeda13be294094475c6f9af79adc035dcb53392
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-01-06 17:31:54 +0900

    2022-01-06-1730

--- SampleSql/AlbumHistory_tb.sql
+++ SampleSql/AlbumHistory_tb.sql
@@ -11,6 +11,15 @@ CREATE TABLE [dbo].[AlbumHistory]
     [lastMod] DATETIME NOT NULL DEFAULT GETDATE(),
 )
 
+-- dbo.AlbumHistory.sql
+CREATE TABLE [dbo].[AlbumHistory] (
+    [history] INT            IDENTITY (1, 1) NOT NULL,
+    [id]      CHAR (5)       NOT NULL,
+    [comment] NVARCHAR (100) NOT NULL,
+    [lastMod] DATETIME       DEFAULT (getdate()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([history] ASC)
+);
+
 -- AlbumDataSet.xsd / AlbumTableAdapter
 INSERT INTO [AlbumHistory] ([Id], [comment], [lastMod])
   VALUES (@Id, @comment, GETDATE())
