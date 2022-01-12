1d880e4394b77a004ce148bedbd7b7f994a63127
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-01-12 15:04:05 +0900

    2022-01-12-1500

---
+++ CS_code/DocTemplate.cs
@@ -0,0 +1,34 @@
+﻿/**
+ *@title SelfAspNet / SampleAsp /
+ *@target
+ *@source
+ *@reference 山田祥寛『独習 ASP.NET 第６版』翔泳社, 2020
+ *@content
+ *@subject
+ *
+ *@author shika
+ *@date
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SelfAspNet.CS_code
+{
+    public class DocTemplate
+    {
+        //static void Main(string[] args)
+        public void Main(string[] args)
+        {
+            //new CsharpBegin.Utility.FileDocumentDiv.FileDocExecute().ReadWriteExe();
+        }//Main()
+    }//class
+}
+
+/*
+【考察】SelfAspNet -> CsharpBegin参照
+クラスそのものは参照可だが、static Main()で実行しても、
+CsharpBegin内には static Main()がないため、ビルドエラーとなる。
+
+ */
