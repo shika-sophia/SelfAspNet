7a10cfa3ff85a1b768312eebe2bda87dfe77b3d7
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-01-16 16:22:16 +0900

    2022-01-16-1630

---
+++ SampleAsp/NT06_ImplicitObject/RequestHeaderSample.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <自動生成>
+//     このコードはツールによって生成されました。
+//
+//     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+//     コードが再生成されるときに損失したりします。 
+// </自動生成>
+//------------------------------------------------------------------------------
+
+namespace SelfAspNet.SampleAsp.NT06_ImplicitObject
+{
+
+
+    public partial class RequestHeaderSample
+    {
+
+        /// <summary>
+        /// formRequestHeaderSample コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm formRequestHeaderSample;
+
+        /// <summary>
+        /// gridReqProp コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gridReqProp;
+
+        /// <summary>
+        /// gridReqPath コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gridReqPath;
+
+        /// <summary>
+        /// gridReqHeader コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gridReqHeader;
+    }
+}
