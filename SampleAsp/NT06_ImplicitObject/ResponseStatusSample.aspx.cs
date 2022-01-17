7afeddb2262b3fd358f6fda5fa34bc8aed5329dc
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-01-17 15:54:20 +0900

    2022-01-17-1600

--- SampleAsp/NT06_ImplicitObject/ResponseStatusSample.aspx.cs
+++ SampleAsp/NT06_ImplicitObject/ResponseStatusSample.aspx.cs
@@ -44,6 +44,7 @@
  *         305: Use Proxy
  *         
  *         ＊400s Client Error
+ *         400: Bad Request
  *         401: Unauthorized
  *         402: Payment Required
  *         403: Forbidden
@@ -93,7 +94,7 @@ namespace SelfAspNet.SampleAsp.NT06_ImplicitObject
                 100, 101,                          //100s Information
                 200, 201, 202, 203, 204, 205, 206, //200s Success
                 300, 301, 302, 303, 304, 305,      //300s Redirect Error
-                401, 402, 403, 404, 405, 406, 407, 408, 409, 410,
+                400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410,
                 411, 412, 413, 414, 415, 416, 417, //400s Client Error
                 500, 501, 502, 503, 504, 505,      //500s Server Error
             };
@@ -137,6 +138,7 @@ namespace SelfAspNet.SampleAsp.NT06_ImplicitObject
 303: See Other
 304: Not Modified
 305: Use Proxy
+400: Bad Request
 401: Unauthorized
 402: Payment Required
 403: Forbidden
