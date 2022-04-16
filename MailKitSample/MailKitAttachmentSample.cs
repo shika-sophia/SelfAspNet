a7559492e503c45a7c1d708c105de7b84793e8b1
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-04-16 11:47:01 +0900

    2022-04-16-1200

---
+++ MailKitSample/MailKitAttachmentSample.cs
@@ -0,0 +1,98 @@
+﻿/*
+ * @title MailKitSample / MailKitAttachmentSample.cs
+ * @see MainMailKitSample.cs
+ * @see Reference_MailKit / MailKit_CsharpMailSend.txt
+ * 
+ * @subject ＊添付ファイルを送信する
+ * Mailkitで添付ファイルを送信するサンプルコードです。
+ * ファイルの添付には、MimeKit.MimePartクラスを使用します。
+ * 
+ * (1) 添付ファイルの設定には、MimeKit.MimePartクラスを使用します。
+ *     コンストラクタには、送信するファイルのMimeTypeを指定し、
+ *   それ以降のプロパティで送信するフィアルの情報を設定していきます。
+ *
+ * (2) メールの本文と添付ファイルを合わせて送る為に、
+ *     MimeKit.Multipartクラスに TextPartと MimePartを設定します。
+ *
+ * (3)メール本文と添付ファイルを設定したMimeKit.Multipartを、
+ *    MimeKit.MimeMessageのBodyに指定して、メールを送信します。
+ *
+ * ファイルの拡張子に合わせたMimeTypeを設定する
+ * 上で紹介したサンプルコードは、送信する添付ファイルのMimeTypeがimage/png固定で書かれおり、
+ * 汎用性がありません。
+ * 複数の拡張子を添付ファイルで送信する場合、
+ * MimeTypeをファイルの拡張子から動的に設定する必要があります。
+ *
+ * MailKitには、ファイルの拡張子からMimeTypeを取得する
+ * MimeKit.MimeTypes.GetMimeTypeメソッドが用意されており、
+ * 以下のコードを書くことで複数の拡張子に対応可能です。
+ *
+ * var filePath = "image.jpg";    
+ * //ファイルの拡張子からMIMEタイプを取得する
+ * var mimeType = MimeKit.MimeTypes.GetMimeType(filePath);  //=> image/jpeg
+ * var attachment = new  MimeKit.MimePart(mimeType);
+ *
+ * MimeKit.MimeTypes.GetMimeTypeメソッドが対応している拡張子は、
+ * MailKitのソースコードに書かれてましたので、以下のリンクに対応している拡張子をまとめました。
+ * MailKitのGetMimeTypeでMIMEタイプに変換できる拡張子一覧
+ * =>〔@see Reference_MailKit / MailKit_CsharpMailSend.txt〕
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SelfAspNet.MailKitSample
+{
+    public class MailKitAttachmentSample
+    {
+        public void MailSenderAttachment()
+        {
+            string filePath = "image.jpg";
+            string fileName = Path.GetFileName(filePath);
+
+            string host = "<smtp server name>";
+            int port = 587;
+
+            using (var smtp = new MailKit.Net.Smtp.SmtpClient())
+            {
+                smtp.Connect(host, port,
+                    MailKit.Security.SecureSocketOptions.Auto);
+                smtp.Authenticate("<id>", "<pass>");
+
+                var mail = new MimeKit.MimeMessage();
+
+                //(0)//ファイルの拡張子からMIMEタイプを取得する
+                string mimeType = MimeKit.MimeTypes.GetMimeType(filePath);  //=> image/jpeg
+
+                //(1)添付ファイルを設定
+                //MimeKit.MimePart attach = new MimeKit.MimePart("image/png");
+                MimeKit.MimePart attach = new MimeKit.MimePart(mimeType);
+
+                attach.Content = new MimeKit.MimeContent(
+                    File.OpenRead(filePath));
+                attach.ContentDisposition = new MimeKit.ContentDisposition();
+                attach.ContentTransferEncoding = MimeKit.ContentEncoding.Base64;
+                attach.FileName = fileName;
+
+                //メールの本文を設定
+                MimeKit.TextPart textPart = new MimeKit.TextPart("plain");
+                textPart.Text = "メール本文\n\n以上";
+
+                //(2)Multipartオブジェクトの作成
+                MimeKit.Multipart multi = new MimeKit.Multipart("mixed");
+                multi.Add(textPart);
+                multi.Add(attach);
+
+                //(3)Bodyに、添付ファイルとメール本文を格納したMultipartオブジェクトを設定
+                mail.Subject = "メール件名";
+                mail.Body = multi;
+                smtp.Send(mail);  //メールを送信
+                smtp.Disconnect(true);
+            }//using
+
+        }//MailSenderAttachment()
+    }//class
+}
---
+++ MailKitSample/MailKitCopySample.cs
@@ -0,0 +1,97 @@
+﻿/**
+ *@title SelfAspNet / MailKitSample / MailKitCopySample.cs
+ *@reference 山田祥寛『独習 ASP.NET 第６版』翔泳社, 2020
+ *@reference ■ Qiita Blog /author: @tkc13687936
+ *           更新日 2020年05月12日
+ *           ◆C#でメール送信する
+ *            https://qiita.com/tkc13687936/items/fbbc1339d0af1c26b004
+ *            =>〔Reference / MailKit_Example.txt〕
+ *            
+ *@content MailKitによるメール送信の汎用コピーコード
+ *@subject
+ *
+ *@see MailKitSample_API.txt
+ *@author shika
+ *@date 2022-04-16
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace SelfAspNet.MailKitSample
+{
+    public class MailKitCopySample
+    {
+        public void MailSenderForCopy()
+        {
+            var host = "smtp server name";
+            var port = 587; // 25 or 465 or 587;
+
+            using (var smtp = new MailKit.Net.Smtp.SmtpClient())
+            {
+                try
+                {
+                    //開発用のSMTPサーバが暗号化に対応していないときは、次の行をコメントアウト
+                    //smtp.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                    smtp.Connect(host, port, MailKit.Security.SecureSocketOptions.Auto);
+                    //認証設定
+                    smtp.Authenticate("id", "password");
+
+                    //送信するメールを作成する
+                    var mail = new MimeKit.MimeMessage();
+                    var builder = new MimeKit.BodyBuilder();
+                    mail.From.Add(new MimeKit.MailboxAddress("", "送る人のメールアドレス"));
+                    mail.To.Add(new MimeKit.MailboxAddress("", "配信先のメールアドレス"));
+                    mail.Subject = "メールタイトル";
+                    MimeKit.TextPart textPart = new MimeKit.TextPart("Plain");
+                    textPart.Text = "メール本文";
+                    string filename = "添付ファイルの名前";
+                    //添付ファイルがあった時の処理
+                    if (!string.IsNullOrEmpty(filename))
+                    {
+                        //Page.Server.MapPath(string) は 「aspx.cs」で有効
+                        //ASP.NET限定「~」アプリルートを表す
+                        //var filePath = Server.MapPath("~/App_Data/doc/" + filename);
+                        var filePath = "/App_Data/doc/" + filename;
+                        var mimeType = MimeKit.MimeTypes.GetMimeType(filePath);
+                        var attachment = new MimeKit.MimePart(mimeType);
+                        using (var file = File.OpenRead(filePath))
+                        {
+                            attachment.Content = new MimeKit.MimeContent(file);
+                            attachment.ContentDisposition = new MimeKit.ContentDisposition();
+                            attachment.ContentTransferEncoding = MimeKit.ContentEncoding.Base64;
+                            attachment.FileName = Path.GetFileName(filePath);
+                            var multipart = new MimeKit.Multipart("mixed");
+                            multipart.Add(textPart);
+                            multipart.Add(attachment);
+                            mail.Body = multipart;
+                            //メールを送信する
+                            smtp.Send(mail);
+                        }//using
+                    }
+                    else
+                    {
+                        var multipart = new MimeKit.Multipart("mixed");
+                        multipart.Add(textPart);
+                        mail.Body = multipart;
+                        //メールを送信する
+                        smtp.Send(mail);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+                finally
+                {
+                    //SMTPサーバから切断する
+                    smtp.Disconnect(true);
+                }
+            }//using
+        }//MailSenderForCopy()
+    }//class
+}
