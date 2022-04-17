7bdf19f2ad899cfda19ee188343fab1d023dc0f5
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-04-17 13:31:11 +0900

    2022-04-17-1330

---
+++ SampleAsp/NT09_RichControl/WizardControl/WizardSample.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" 
+    CodeBehind="WizardSample.aspx.cs" 
+    Inherits="SelfAspNet.SampleAsp.NT09_RichControl.WizardControl.WizardSample" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>WizardSample</title>
+</head>
+<body>
+<form id="formWizardSample" runat="server">
+<div>
+<asp:Wizard ID="wiz" runat="server"  OnFinishButtonClick="Wizard1_FinishButtonClick" ActiveStepIndex="0" FinishPreviousButtonText="&lt; Prev" StartNextButtonText="Next &gt;" StepNextButtonText="Next &gt;" StepPreviousButtonText="&lt; Prev" >
+<WizardSteps>
+    <asp:WizardStep runat="server" title="個人情報">
+    Name: <br />
+    <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="reqName" runat="server"
+        ErrorMessage="*"></asp:RequiredFieldValidator><br />
+    Email:<br />
+    <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="reqEmail" runat="server"
+        ErrorMessage="*"></asp:RequiredFieldValidator>
+    <asp:RegularExpressionValidator ID="regexEmail" runat="server"
+        ErrorMessage="*"></asp:RegularExpressionValidator><br />
+    Email: (confirm)<br />
+    <asp:TextBox ID="txtEmail2" runat="server"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="reqEmail2" runat="server" 
+        ErrorMessage="*"></asp:RequiredFieldValidator>
+    <asp:CompareValidator ID="compareEmail2" runat="server" 
+        ErrorMessage="*"></asp:CompareValidator><br />
+    <br />
+    </asp:WizardStep>
+    <asp:WizardStep runat="server" StepType="Step" title="書籍感想">
+    </asp:WizardStep>
+    <asp:WizardStep runat="server" StepType="Finish" title="自由欄">
+    </asp:WizardStep>
+    <asp:WizardStep runat="server" StepType="Complete" title="Thank You.">
+    </asp:WizardStep>
+</WizardSteps>
+</asp:Wizard>
+<asp:SqlDataSource ID="sdsWiz" runat="server">
+</asp:SqlDataSource>
+<asp:ValidationSummary ID="summaryWiz" runat="server"  />
+</div>
+</form>
+</body>
+</html>
---
+++ SampleAsp/NT09_RichControl/WizardControl/WizardSample.aspx.designer.cs
@@ -0,0 +1,125 @@
+﻿//------------------------------------------------------------------------------
+// <自動生成>
+//     このコードはツールによって生成されました。
+//
+//     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+//     コードが再生成されるときに損失したりします。 
+// </自動生成>
+//------------------------------------------------------------------------------
+
+namespace SelfAspNet.SampleAsp.NT09_RichControl.WizardControl
+{
+
+
+    public partial class WizardSample
+    {
+
+        /// <summary>
+        /// formWizardSample コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm formWizardSample;
+
+        /// <summary>
+        /// wiz コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Wizard wiz;
+
+        /// <summary>
+        /// txtName コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtName;
+
+        /// <summary>
+        /// reqName コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqName;
+
+        /// <summary>
+        /// txtEmail コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// reqEmail コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqEmail;
+
+        /// <summary>
+        /// regexEmail コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator regexEmail;
+
+        /// <summary>
+        /// txtEmail2 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail2;
+
+        /// <summary>
+        /// reqEmail2 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqEmail2;
+
+        /// <summary>
+        /// compareEmail2 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator compareEmail2;
+
+        /// <summary>
+        /// sdsWiz コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource sdsWiz;
+
+        /// <summary>
+        /// summaryWiz コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary summaryWiz;
+    }
+}
