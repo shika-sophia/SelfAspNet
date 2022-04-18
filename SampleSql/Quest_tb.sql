8a9f15adb21ae795e032fb54592482a795812022
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-04-18 14:02:18 +0900

    2022-04-18-1400

--- SampleSql/Quest_tb.sql
+++ SampleSql/Quest_tb.sql
@@ -1,14 +1,18 @@
 ﻿/*
-CREATE TABLE [dbo].[Quest]
-(
-	[qid] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [name] NVARCHAR(100) NULL, 
-    [email] NVARCHAR(255) NULL, 
-    [purpose] NVARCHAR(30) NULL, 
-    [free] NVARCHAR(MAX) NULL, 
-    [mailNews] BIT NULL
-)
-
-GO
+CREATE TABLE [dbo].[Quest] (
+    [qid]      INT            IDENTITY (1, 1) NOT NULL,
+    [name]     NVARCHAR (100) NULL,
+    [email]    NVARCHAR (255) NULL,
+    [purpose]  NVARCHAR (30)  NULL,
+	[evalute] NVARCHAR (15),
+    [free]     NVARCHAR (MAX) NULL,
+    [mailNews] BIT            NULL,
+    PRIMARY KEY CLUSTERED ([qid] ASC)
+);
+*/
+/*
+-- Inserted Data by Wizard Sample
+1	shika	dammy@example.com	Reserch	Good
+Gof Design Pattern	True
 */
 
