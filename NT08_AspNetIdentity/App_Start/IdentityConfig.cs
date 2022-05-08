e489a44c72eda76d9f42eb1e4ccb00b6b0762a97
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-05-09 03:42:19 +0900

    2022-05-09-0330

---
+++ NT08_AspNetIdentity/App_Start/IdentityConfig.cs
@@ -0,0 +1,131 @@
+﻿/*
+ * @modified 8.7 Role
+ * @see SampleUser/RoleAuthorizeDiv/RoleSeting.txt
+ */
+
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using NT08_AspNetIdentity.Models;
+
+namespace NT08_AspNetIdentity
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // 電子メールを送信するには、電子メール サービスをここにプラグインします。
+            return Task.FromResult(0);
+        }
+    }
+
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // テキスト メッセージを送信するには、SMS サービスをここにプラグインします。
+            return Task.FromResult(0);
+        }
+    }
+
+    // このアプリケーションで使用されるアプリケーション ユーザー マネージャーを設定します。UserManager は ASP.NET Identity の中で定義されており、このアプリケーションで使用されます。
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+        {
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            // ユーザー名の検証ロジックを設定します
+            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+
+            // パスワードの検証ロジックを設定します
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,
+            };
+
+            // 2 要素認証プロバイダーを登録します。このアプリケーションでは、電話とメールをユーザー確認用コード受け取りのステップとして使用します
+            // 独自のプロバイダーを作成して、ここにプラグインすることができます。
+            manager.RegisterTwoFactorProvider("コードを電話で伝える", new PhoneNumberTokenProvider<ApplicationUser>
+            {
+                MessageFormat = "セキュリティ コードは {0} です"
+            });
+            manager.RegisterTwoFactorProvider("コードをメールで送信する", new EmailTokenProvider<ApplicationUser>
+            {
+                Subject = "セキュリティ コード",
+                BodyFormat = "セキュリティ コードは {0} です"
+            });
+
+            // ユーザー ロックアウトの既定値を設定
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+
+            manager.EmailService = new EmailService();
+            manager.SmsService = new SmsService();
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider = new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+            return manager;
+        }
+    }//class ApplicationUserManager
+
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    {
+        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager) :
+            base(userManager, authenticationManager) { }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
+        }
+    }//class ApplicationSignInManager
+
+    public class ApplicationRoleManager : RoleManager<ApplicationRole, string>
+    {
+        public ApplicationRoleManager(
+            IRoleStore<ApplicationRole, string> store) : base(store) { }
+
+        public static ApplicationRoleManager Create(
+            IdentityFactoryOptions<ApplicationRoleManager> options, 
+            IOwinContext context)
+        {
+            var manager = new ApplicationRoleManager(
+                new RoleStore<ApplicationRole>(
+                    context.Get<ApplicationDbContext>() 
+                )
+            );
+
+            if (!manager.RoleExists("Admin"))
+            {
+                manager.Create(new ApplicationRole { Name = "Admin" });
+            }
+
+            return manager;
+        }//Create()
+    }//class ApplicationRoleManager
+}
