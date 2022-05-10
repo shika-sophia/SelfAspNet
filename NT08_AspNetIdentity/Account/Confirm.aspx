39af69f503020f6efb85286baca86f76cee70e9e
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-05-10 13:57:08 +0900

    2022-05-10-1400

---
+++ NT08_AspNetIdentity/Account/Confirm.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="アカウントの確認" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Confirm.aspx.cs" Inherits="NT08_AspNetIdentity.Account.Confirm" Async="true" %>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <h2><%: Title %>。</h2>
+
+    <div>
+        <asp:PlaceHolder runat="server" ID="successPanel" ViewStateMode="Disabled" Visible="true">
+            <p>
+                アカウントをご確認いただき、ありがとうごいます。ログインするには <asp:HyperLink ID="login" runat="server" NavigateUrl="~/Account/Login">こちら</asp:HyperLink>  をクリックしてください             
+            </p>
+        </asp:PlaceHolder>
+        <asp:PlaceHolder runat="server" ID="errorPanel" ViewStateMode="Disabled" Visible="false">
+            <p class="text-danger">
+                エラーが発生しました。
+            </p>
+        </asp:PlaceHolder>
+    </div>
+</asp:Content>
---
+++ NT08_AspNetIdentity/Account/Confirm.aspx.cs
@@ -0,0 +1,43 @@
+﻿/*
+ *@modified 8.7 Role
+ *          IdentityResult manager.AddToRole(string userId, string role);
+ *@see ~/SampleUser/RoleAutorizeDiv/RoleSetting.txt
+ */
+using System;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using NT08_AspNetIdentity.Models;
+
+namespace NT08_AspNetIdentity.Account
+{
+    public partial class Confirm : Page
+    {
+        protected string StatusMessage
+        {
+            get;
+            private set;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string code = IdentityHelper.GetCodeFromRequest(Request);
+            string userId = IdentityHelper.GetUserIdFromRequest(Request);
+            if (code != null && userId != null)
+            {
+                var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var result = manager.ConfirmEmail(userId, code);
+                if (result.Succeeded)
+                {
+                    manager.AddToRole(userId, "Admin"); //modified above
+                    successPanel.Visible = true;
+                    return;
+                }
+            }
+            successPanel.Visible = false;
+            errorPanel.Visible = true;
+        }
+    }
+}
