17063a80ac8763fb21e6c5b07ba42ec38924ddd8
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-05-12 16:50:09 +0900

    2022-05-12-1650

--- SampleAsp/NT10_FlagmentObject/Global_asax_Div/ErrorSample.aspx.cs
+++ SampleAsp/NT10_FlagmentObject/Global_asax_Div/ErrorSample.aspx.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿/**
+ * @modified 10.4.3 Error Page
+ *           ConnectionString="<%$ ConnectionStrings:SelfAspError %>" 
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-/**
- * @modified 10.4.3 Error Page
- *           ConnectionString="<%$ ConnectionStrings:SelfAspError %>" 
- */
 namespace SelfAspNet.SampleAsp.NT10_FlagmentObject.Global_asax_Div
 {
     public partial class ErrorSample : System.Web.UI.Page
