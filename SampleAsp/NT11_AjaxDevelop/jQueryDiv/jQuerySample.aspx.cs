dfcc857e266055ad0dd6e919d3ef50e77c148459
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-05-29 03:19:09 +0900

    2022-05-29-0300

--- SampleAsp/NT11_AjaxDevelop/jQueryDiv/jQuerySample.aspx.cs
+++ SampleAsp/NT11_AjaxDevelop/jQueryDiv/jQuerySample.aspx.cs
@@ -1,10 +1,21 @@
 ﻿/**
  *@title SelfAspNet / SampleAsp / NT11_AjaxDevelop
- *       jQueryDiv / jQuerySample.aspx
- *@target
- *@source
+ *       jQueryDiv / jQuerySample.aspx.cs
+ *@target jQuerySample.aspx
+ *@source https://wings.msn.to/books/{0}/{0}.jpg | {0}: isbn
+ *
  *@reference 山田祥寛『独習 ASP.NET 第６版』翔泳社, 2020
- *@content
+ *@content jQueryの利用
+ *
+ *@prepare 事前にインストールが必要だが、ValidatorControlで実行済
+ *@prepare jQueryをインストール
+ *         PM> Install jquery
+ *         -> 「~/Scripts」に jquery-x.x.x.xxx.js 
+ *         => 〔NT03_ServerControl / ValidSample.aspx.cs〕
+ *         
+ *@prepare 「Global.asax」Application_Start()
+ *          => jQueryの保存場所を登録 〔同上 | p100 | NT20〕
+ *          
  *@subject
  *
  *@see jQuerySample_withFadeout.jpg
