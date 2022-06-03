1e0263d68676cd83f0fa43f383d09f992ed85f04
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-06-04 05:57:42 +0900

    2022-06-04-0600

---
+++ SelfAspNetApi/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿/*
+ *@see SampleAspNetApi / SelfAspNetApi_Setting.txt
+ *
+ */
+
+using SelfAspNetApi.Models;
+using System.Net;
+using System.Web.Http;
+
+namespace SelfAspNetApi.Controllers
+{
+    public class BookController : ApiController
+    {
+        public Book GetBook(string id)
+        {
+            var db = new SelfAspNetApiDB();
+            Book book = db.Book.Find(id);
+
+            if(book == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return book;
+        }//GetBook()
+
+    }//class
+}
