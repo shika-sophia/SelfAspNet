9a108c78d2b14dd8c7237ddc322d0093f6d58370
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-06-06 02:38:51 +0900

    2022-06-06-0230

--- SelfAspNetApi/Global.asax.cs
+++ SelfAspNetApi/Global.asax.cs
@@ -1,6 +1,13 @@
-﻿using System;
+﻿/*
+ *@see SelfAspNetApi / SampleCode / SelfAspNetApi_Setting.txt
+ *@modified SelfAspNetApi / SampleCode / ClientCallJs.aspx.cs
+ *@modified SelfAspNetApi / SampleCode / ClientBundleJs.aspx.cs
+ */
+
+using System;
 using System.Web;
 using System.Web.Http;
+using System.Web.Optimization;
 using System.Web.UI;
 
 namespace SelfAspNetApi
@@ -12,7 +19,7 @@ namespace SelfAspNetApi
             // アプリケーションのスタートアップで実行するコードです
             GlobalConfiguration.Configure(WebApiConfig.Register);
 
-            //jQueryの登録
+            //---- jQueryの登録 ----
             ScriptManager.ScriptResourceMapping.AddDefinition(
                 "jquery", null,
                 new ScriptResourceDefinition()
@@ -22,6 +29,14 @@ namespace SelfAspNetApi
                     CdnPath = "http://ajax.microsoft.com/ajax/jQuery/jquery-3.6.0.min.js",
                     CdnDebugPath = "http://ajax.microsoft.com/ajax/jQuery/jquery-3.6.0.js"
                 });
-        }
+
+            //---- Bundle ----
+            BundleTable.Bundles.Add(
+                new ScriptBundle("~/bundles/mySetting")
+                .Include("~/SampleCode/BookSearch.js")
+            );
+
+            BundleTable.EnableOptimizations = true;
+        }//Application_Start()
     }//class
 }
