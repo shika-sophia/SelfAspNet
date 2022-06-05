cd56e2ccd6e68915d9413f84792a2cb7b3256756
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-06-05 17:59:42 +0900

    2022-06-05-1745

---
+++ SelfAspNetApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SelfAspNetApi.Global" Language="C#" %>
---
+++ SelfAspNetApi/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+using System.Web.UI;
+
+namespace SelfAspNetApi
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // アプリケーションのスタートアップで実行するコードです
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            //jQueryの登録
+            ScriptManager.ScriptResourceMapping.AddDefinition(
+                "jquery", null,
+                new ScriptResourceDefinition()
+                {
+                    Path = "~/Scripts/jquery-3.6.0.min.js",
+                    DebugPath = "~/Scripts/jquery-3.6.0.js",
+                    CdnPath = "http://ajax.microsoft.com/ajax/jQuery/jquery-3.6.0.min.js",
+                    CdnDebugPath = "http://ajax.microsoft.com/ajax/jQuery/jquery-3.6.0.js"
+                });
+        }
+    }//class
+}
